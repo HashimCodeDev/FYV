fe136b3b28f939d8a10934c83fae0078d19807d7
Hashim <hashimmohammed212@gmail.com>
2025-01-11 00:07:10 +0530

    new home page

--- Frontend/src/pages/homepage.js
+++ Frontend/src/pages/homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
@@ -59,8 +59,80 @@ const Homepage = () => {
     );
   };
 
+  const Emoticons = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (event) => {
+      // Update the position based on mouse movement
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+
+      // Get the emoticons container
+      const emoticonsContainer = document.querySelector('.emoticons');
+
+      // Calculate the movement of the container based on the mouse position
+      const diffX = mouseX - window.innerWidth / 2; // Move based on the difference from the center
+      const diffY = mouseY - window.innerHeight / 2; // Move based on the difference from the center
+
+      // Apply the movement to the container and move gifs inside it
+      emoticonsContainer.style.transform = `translate(
+      ${diffX / 20}px, 
+      ${diffY / 20}px)`;
+    };
+
+    return (
+      <div
+        className='emoticon-container'
+        onMouseMove={handleMouseMove}>
+        <div
+          className='emoticons'
+          style={{
+            transform: `translate(${position.x}px, ${position.y}px)`,
+          }}>
+          <img
+            className='cool'
+            src='/external/cool.gif'
+          />
+          <img
+            className='lol'
+            src='/external/lol.gif'
+          />
+          <img
+            className='messages'
+            src='/external/messages.gif'
+          />
+          <img
+            className='happy'
+            src='/external/happy.gif'
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const Languages = () => {
+    return (
+      <div className='languages'>
+        <h2>BUILT WITH: </h2>
+        <img
+          className='react'
+          src='/external/react-js.svg'
+        />
+        <img
+          className='node'
+          src='/external/node-js.svg'
+        />
+        <img
+          className='express'
+          src='/external/express-js.svg'
+        />
+      </div>
+    );
+  };
+
   return (
     <div className='homepage-container'>
+      <Emoticons />
       <div className='header'>
         <Logo />
         <NavBar />
@@ -69,6 +141,10 @@ const Homepage = () => {
 
       <div className='hero'>
         <Heading />
+      </div>
+
+      <div className='language-container'>
+        <Languages />
       </div>
     </div>
   );
