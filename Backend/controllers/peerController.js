16be14ac566db298892e4c3b2b2fa2cc4a6c4328
Hashim <hashimmohammed212@gmail.com>
2025-01-19 21:51:25 +0530

    code restructuring

--- Backend/controllers/peerController.js
+++ Backend/controllers/peerController.js
@@ -56,71 +56,67 @@ exports.joinRoom = async (req, res) => {
 exports.matchMake = async (req, res) => {
   const { userId } = req.body;
   console.log('User trying to match:', userId);
+  let connectionSnapshot;
 
   try {
     console.log('Finding Match');
-    const connectionSnapshot = await db
+    connectionSnapshot = await db
       .collection('connection')
       .where('status', '==', 2)
       .where('userId', '!=', userId)
       .get();
     console.log('Users Found', connectionSnapshot.docs[0]);
-
-    if (connectionSnapshot.empty) {
-      console.error('No matching documents.');
-      return res.status(200).json({ error: 'No users found for matching' });
-    }
-    const users = connectionSnapshot.docs.map((doc) => ({
-      userid: doc.userId,
-      ...doc.data(),
-    }));
-    const randomUser = users[Math.floor(Math.random() * users.length)];
-
-    // Update user statuses to matched (1) in both sessions and matching collections
-    const batch = db.batch();
-
-    // Find the document with the userId in the 'connection' collection
-    const userDocRef = db
-      .collection('connection')
-      .where('userId', '==', userId);
-    const randomUserDocRef = db
-      .collection('connection')
-      .where('userId', '==', randomUser.userid);
-
-    try {
-      // Get the snapshot of the documents for both userId and randomUser.userid
-      const userSnapshot = await userDocRef.get();
-      const randomUserSnapshot = await randomUserDocRef.get();
-
-      if (!userSnapshot.empty && !randomUserSnapshot.empty) {
-        // Update the status of the matched user documents
-        userSnapshot.forEach((doc) => {
-          batch.update(doc.ref, { status: 1 });
-        });
-
-        randomUserSnapshot.forEach((doc) => {
-          batch.update(doc.ref, { status: 1 });
-        });
-
-        // Commit the batch update
-        await batch.commit();
-        console.log('Status updated for both users');
-      } else {
-        console.error(
-          'No matching documents found for userId or randomUser.userid'
-        );
-      }
-    } catch (error) {
-      console.error('Error updating status:', error);
-    }
-
-    console.log(randomUser);
-
-    res
-      .status(200)
-      .json({ message: 'Users matched', userId, remoteId: randomUser.peerId });
   } catch (error) {
     console.error('Error matching users:', error);
     res.status(500).json({ error: 'Server error' });
   }
+
+  if (connectionSnapshot && connectionSnapshot.empty) {
+    console.error('No matching documents.');
+    return res.status(200).json({ error: 'No users found for matching' });
+  }
+  const users = connectionSnapshot.docs.map((doc) => ({
+    userid: doc.userId,
+    ...doc.data(),
+  }));
+  const randomUser = users[Math.floor(Math.random() * users.length)];
+
+  // Update user statuses to matched (1) in both sessions and matching collections
+  const batch = db.batch();
+
+  // Find the document with the userId in the 'connection' collection
+  const userDocRef = db.collection('connection').where('userId', '==', userId);
+  const randomUserDocRef = db
+    .collection('connection')
+    .where('userId', '==', randomUser.userid);
+
+  try {
+    // Get the snapshot of the documents for both userId and randomUser.userid
+    const userSnapshot = await userDocRef.get();
+    const randomUserSnapshot = await randomUserDocRef.get();
+
+    if (!userSnapshot.empty && !randomUserSnapshot.empty) {
+      // Update the status of the matched user documents
+      userSnapshot.forEach((doc) => {
+        batch.update(doc.ref, { status: 1 });
+      });
+
+      randomUserSnapshot.forEach((doc) => {
+        batch.update(doc.ref, { status: 1 });
+      });
+
+      // Commit the batch update
+      await batch.commit();
+      console.log('Status updated for both users');
+    } else {
+      console.error(
+        'No matching documents found for userId or randomUser.userid'
+      );
+    }
+  } catch (error) {
+    console.error('Error updating status:', error);
+  }
+  res
+    .status(200)
+    .json({ message: 'Users matched', userId, remoteId: randomUser.peerId });
 };
